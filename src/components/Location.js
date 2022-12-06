99045b816180329f676c23d0dc13375077cfeebe
joe <joe.ulica@gmail.com>
2022-12-06 16:20:42 -0500

    add prop content to Location.js

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,17 @@
+import pin from "../images/map_pin.png"
+
 export default function Location(location) {
   return (
     <section className="location">
-      <p>{location.title}</p>
+      <img src={location.image} alt={location.title} className="location-photo" />
+      <section className="location-text-block">
+        <img src={pin} alt="pin" className="pin" />
+        <p className="country">{location.country}</p>
+        <a href={location.googleMapsLink}>View on google maps</a>
+        <h2 className="location-title">{location.title}</h2>
+        <p className="travel-dates">{location.startDate} - {location.endDate}</p>
+        <p className="location-description">{location.description}</p>
+      </section>
     </section>
   )
 }
