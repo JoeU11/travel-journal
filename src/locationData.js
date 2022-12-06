690752b60c6978dbf66e52462a9602e936069ae9
joe <joe.ulica@gmail.com>
2022-12-06 15:45:04 -0500

    mock up location data

---
+++ src/locationData.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: "Mount Fuji",
+    country: "Japan",
+    googleMapsLink: "",
+    startDate: Date(2021, 1, 12),
+    endDate: Date(2021, 1, 24),
+    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    image: "https://source.unsplash.com/WLxQvbMyfas"
+  },
+  {
+    title: "Sydney",
+    country: "Australia",
+    googleMapsLink: "",
+    startDate: Date(2021, 5, 27),
+    endDate: Date(2021, 6, 8),
+    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    image: "https://source.unsplash.com/JmuyB_LibRo"
+  },
+  {
+    title: "Geirangerfjord",
+    country: "Norway",
+    googleMapsLink: "",
+    startDate: Date(2021, 10, 1),
+    endDate: Date(2021, 11, 18),
+    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    image: "https://source.unsplash.com/3PeSjpLVtLg"
+  }
+]
